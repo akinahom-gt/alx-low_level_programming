ffaaf8b42b70873869a5fbce5b4d08db8e3103cc
akinahom-gt <akinahomgetahun@gmail.com>
2023-08-16 07:53:43 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * _islower - checks for lowercase character.
+ *
+ *
+ * Return: 1 if lowercase else 0.
+ */
+int _islower(int ch)
+{
+	if (ch >= 97 && ch < 123)
+		return (1);
+	else
+		return (0);
+}
+
